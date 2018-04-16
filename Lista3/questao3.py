1d0740b1e33ea2a7f0826cb0c407aafa267500cd
odran <odrand@users.noreply.github.com>
2018-04-16 11:57:59 -0300

    ...

--- Lista3/questao3.py
+++ Lista3/questao3.py
@@ -19,8 +19,11 @@ def checkUrl(url):
         return False
 
 
+a = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+
 def gerar_string(k):
-    a = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    global a
     ks = list(range(k, 0, -1))
     k = np.random.choice(ks, 1)
     s = np.random.choice(a, k)
@@ -34,7 +37,7 @@ def gerar_url(k):
 def avaliar_indicadora(ss):
     print("Avaliando URLs.")
     import multiprocessing.dummy
-    pool = multiprocessing.dummy.Pool(processes=1000)
+    pool = multiprocessing.dummy.Pool(processes=5000)
     i = pool.map(checkUrl, ss)
     pool.close()
     print("URLs avaliadas.")
@@ -61,10 +64,8 @@ def contagem_cumulativa_da_indicadora_por_n(l):
     return l2
 
 
-def estimar_multi(n_ini, n_fim, k=3):
+def estimar_multi(n_ini, n_fim, k):
     ps = [gerar_url(k) for i in range(n_ini, n_fim)]
-    # ps.append("http://www.ccs.ufrj.br/")
-    # ps.append("http://www.ccs.ufrj.br/")
     # pprint(ps)
     ia = avaliar_indicadora(ps)
     # pprint([e for e in ia if e])
@@ -73,7 +74,7 @@ def estimar_multi(n_ini, n_fim, k=3):
     return ns
 
 
-def card_D(k=3):
+def card_D(k):
     c = 0
     for ki in range(k, 0, -1):
         c += 26**ki
@@ -81,19 +82,15 @@ def card_D(k=3):
     return c
 
 
-def plot(n):
-    es = estimar_multi(1, n)
-    es = np.multiply(es, card_D())
+def plot(n, k=4):
+    es = estimar_multi(1, n, k=k)
+    es = np.multiply(es, card_D(k=k))
     # pprint([e for e in es if e])
-
-    plt.plot(es)  # , basex=2, basey=2)
+    plt.plot(es)
     plt.grid(True)
-    #
+
     plt.show()
 
 
 if __name__ == '__main__':
-    # print(checkUrl('http://www.ccmn.ufrj.br'))  # True
-    # print(checkUrl('http://www.teste.ufrj.br'))  # False
-
     plot(10**4)
