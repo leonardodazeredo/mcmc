35fb890ba93b306fba277581050312da26a1a060
odran <odrand@users.noreply.github.com>
2018-05-20 21:17:37 -0300

    ..

--- Lista4/questao1.py
+++ Lista4/questao1.py
@@ -46,9 +46,10 @@ def calcular_vetor_pi(p0, p1, p2):
 
 
 if __name__ == '__main__':
-    # pprint(matrizP())
-    # for l in matrizP():
-    #     print(round(sum(l), 6))
+    P = matrizP(0.5, 0.3, 0.1)
+    pprint(P)
+    for l in P:
+        print(round(sum(l), 6))
 
     # ps = [0.0, 0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95, 1.0]
     ps = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
