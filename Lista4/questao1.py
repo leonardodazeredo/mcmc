ee3110495b403029d72e232acc0806911a0c614b
odran <odrand@users.noreply.github.com>
2018-05-20 21:14:39 -0300

    ...

--- Lista4/questao1.py
+++ Lista4/questao1.py
@@ -1,29 +1,44 @@
 from pprint import pprint
 import numpy as np
+from copy import deepcopy
 
 
-def roundM(A):
+def matrizP(p0, p1, p2):
+    P = [
+        [(1 - p0**2), 0, 0, 0, p0**2, 0, 0, 0, 0],
+        [p1 * (1 - p0), (1 - p1), 0, 0, 0, p1 * p0, 0, 0, 0],
+        [p2 * (1 - p0), 0, (1 - p2), 0, 0, p2 * p0, 0, 0, 0],
+        [p1 * (1 - p0), 0, 0, (1 - p1), 0, 0, 0, p1 * p0, 0],
+        [0, (p1 - p1**2), 0, (p1 - p1**2), (1 - p1)**2, 0, 0, 0, p1**2],
+        [0, 0, p1 * (1 - p2), p2 * (1 - p1), 0, (1 - p1 - p2 + p1 * p2), 0, 0, p2 * p1],
+        [p2 * (1 - p0), 0, 0, 0, 0, 0, (1 - p2), p2 * p0, 0],
+        [0, p2 * (1 - p1), 0, 0, 0, 0, p1 * (1 - p2), (1 - p1 - p2 + p1 * p2), p2 * p1],
+        [0, 0, (p2 - p2**2), 0, 0, 0, (p2 - p2**2), 0, (2 * (p2**2) - 2 * p2 + 1)],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1]
+    ]
+    return roundM(P)
+
+
+def matrizA(p0, p1, p2):
+    P = matrizP(p0, p1, p2)
+    A = deepcopy(P)
+    for i, l in enumerate(A):
+        if i < len(l):
+            l[i] -= 1
+    return roundM(A)
+
+
+def roundM(A, n=6):
     ml = []
     for l in A:
-        ml.append(list(map((lambda x: round(x, 4)), l)))
+        ml.append(list(map((lambda x: round(x, n)), l)))
     return ml
 
 
-def calcular_vetor_pi(p0=0.5, p1=0.3, p2=0.1):
-    A = [
-        [(1 - p0**2 - 1), 0, 0, 0, p0**2, 0, 0, 0, 0],
-        [p1 * (1 - p0), (1 - p1 - 1), 0, 0, 0, p1 * p0, 0, 0, 0],
-        [p2 * (1 - p0), 0, (1 - p2 - 1), 0, 0, p2 * p0, 0, 0, 0],
-        [p1 * (1 - p0), 0, 0, (1 - p1 - 1), 0, 0, 0, p1 * p0, 0],
-        [0, (p1 - p1**2), 0, (p1 - p1**2), (1 - p1)**2 - 1, 0, 0, 0, p1**2],
-        [0, 0, p1 * (1 - p2), p2 * (1 - p1), 0, (1 - p1 - p2 + p1 * p2 - 1), 0, 0, p2 * p1],
-        [p2 * (1 - p0), 0, 0, 0, 0, 0, (1 - p2 - 1), p2 * p0, 0],
-        [0, p2 * (1 - p1), 0, 0, 0, 0, p1 * (1 - p2), (1 - p1 - p2 + p1 * p2 - 1), p2 * p1],
-        [0, 0, (p2 - p2**2), 0, 0, 0, (p2 - p2**2), 0, (2 * (p2**2) - 2 * p2 + 1 - 1)],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1]
-    ]
+def calcular_vetor_pi(p0, p1, p2):
+    A = matrizA(p0, p1, p2)
 
-    A = np.matrix(roundM(A))
+    A = np.matrix(A)
     B = np.matrix([0, 0, 0, 0, 0, 0, 0, 0, 0, 1])
 
     pi = A.I * B.transpose()
@@ -31,6 +46,10 @@ def calcular_vetor_pi(p0=0.5, p1=0.3, p2=0.1):
 
 
 if __name__ == '__main__':
+    # pprint(matrizP())
+    # for l in matrizP():
+    #     print(round(sum(l), 6))
+
     # ps = [0.0, 0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95, 1.0]
     ps = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
     for p0 in ps:
