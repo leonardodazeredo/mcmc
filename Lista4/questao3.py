bfb6847e25931b8f361bb4ec564b380debbffa1a
odran <odrand@users.noreply.github.com>
2018-05-22 04:30:16 -0300

    ...

--- Lista4/questao3.py
+++ Lista4/questao3.py
@@ -19,15 +19,23 @@ def matrizPa(n):
 
 def matrizPb(n):
     A = np.matrix(np.zeros([n, n]))
-    i = 0
+    A[0, 0] = 1 / 2
+    A[0, 1] = A[0, 2] = 1 / 4
+    A[1, 0] = 1 / 6
+    i = 1
     while i < n:
         g = int((2 * (i + 1)))
         if g <= n - 1:
-            A[i, g - 1] = A[i, g] = A[i + 1, int(i / 2)] = round(1 / 6, 4)
+            A[i, g - 1] = A[i, g] = A[i + 1, int(i / 2)] = 1 / 6
         elif i < n - 1:
-            A[i + 1, int(i / 2)] = round(1 / 2, 4)
-        A[i, i] = round(1 / 2, 4)
+            A[i + 1, int(i / 2)] = 1 / 2
+        if g == n - 1:
+             A[i + 1, int(i / 2)] = 1 / 2
+        A[i, i] = 1 / 2
         i += 1
+
+    for l in A:
+        print(round(np.sum(l), 6))
     return A
 
 
@@ -40,33 +48,38 @@ def matrizPb(n):
 #             A[i, g - 1] = A[i, g] = A[i + 1, int(i / 2)] = round(1 / 6, 4)
 #         elif i < n - 1:
 #             A[i + 1, int(i / 2)] = round(1 / 2, 4)
-#         A[i, i] = round(1 / 2, 4)
+#         # A[i, i] = round(1 / 2, 4)
 #         i += 1
 #     return A
 
-def roundM(A, n=6):
-    ml = []
-    for l in A:
-        ml.append(list(map((lambda x: round(x, n)), l)))
-    return ml
 
+def calcular_vetor_pi_iter(n):
+    P = matrizPa(n)
+    pi0 = np.matrix(np.zeros(n))
+    pi0[0, 0] = 1
 
-def calcular_vetor_pi_iter(p0, p1, p2):
-    P = matrizP(p0, p1, p2)
-    P = np.matrix(P)
+    print(P)
 
-    pi0 = np.matrix([0, 0, 0, 0, 1, 0, 0, 0, 0])
+    # pprint(pi0)
 
     R = pi0 * P
+
+    # pprint(R.transpose())
+
     i = 0
     result = 100
-    while result > 10**-10:
+    while result > 10**-6:
         Rn = R * P
         result = (np.sum(np.absolute(R - Rn))) / 2
         R = Rn
+        # pprint(R.transpose())
+        # print(np.sum(R))
         i += 1
     return R.transpose()
 
 
 if __name__ == '__main__':
-    pprint(matrizPb(15))
+    pi = calcular_vetor_pi_iter(15)
+    #
+    pprint(pi)
+    print(np.sum(pi))
